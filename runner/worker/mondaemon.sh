b6ebd8372b37a87b4c93b439ed38d066acefc63f
Aaron McPhall <amcphall@gmail.com>
2014-07-06 17:45:46 -0600

    more code

--- runner/worker/mondaemon.sh
+++ runner/worker/mondaemon.sh
@@ -5,6 +5,8 @@ RUNNING="RUNNING"
 
 STATUS_FILE=/tmp/runner.status
 MNT_DEST="/mnt"
+TIMEOUT=120
+STRACE_CMD="timeout $TIMEOUTE -s KILL strace -o $MNT_DEST/strace -f -e trace=file -u nobody"
 
 check_new() {
     mnt_line=$(blkid | grep LABEL="RUN_THIS")
@@ -27,7 +29,24 @@ update_status() {
 }
 
 run_code() {
+    if [ ! -x "$MNT_DEST/prog" ]; then
+        echo "No program provided" 1>&2
+        return 1
+    fi
+    if [ -e "$MNT_DEST/0" ]; then
+        cat $MNT_DEST/0 | $STRACE_CMD $MNT_DEST/prog 1> $MNT_DEST/1 2> $MNT_DEST/2
+    else
+        $STRACE_CMD $MNT_DEST/prog 1> $MNT_DEST/1 2> $MNT_DEST/2
+    fi
+}
 
+copy_files() {
+    count=0
+    for f in $(grep -o -P 'open\("[^"]*".*WR' $MNT_DEST/strace | cut -d\" -f2); do
+        mkdir -p $MNT_DEST/files/$count
+        cp $f $MNT_DEST/files/$count
+        count=$((count+1))
+    done
 }
 
 main_loop() {
