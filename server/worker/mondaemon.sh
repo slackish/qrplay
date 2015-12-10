c0eeff831265cf4b4d2a54dd0b7290cc89368010
Aaron McPhall <amcphall@lanl.gov>
2015-12-10 09:08:30 -0700

    updated mondaemon to include attaching a data partition, cleaned up code

--- server/worker/mondaemon.sh
+++ server/worker/mondaemon.sh
@@ -1,26 +1,70 @@
 #!/bin/bash
+#
+# Find new material to run, and execute it.
+#
+# It's expecting one or two filesystems that will be labeled "RUN_THIS" and 
+# "DATA".
+# DATA is the optional partition to allow data to be fed into the program.
+# RUN_THIS is rw filesystem that should contain an executable named "prog" and 
+# optionally a file named 0 for stdin.
+# 
+# To avoid race conditions, attach DATA before attaching RUN_THIS
+#
 
 WAITING="WAITING"
 RUNNING="RUNNING"
 
 STATUS_FILE=/tmp/runner.status
-MNT_DEST="/mnt"
+RUN_THIS_DEST="/mnt"
+DATA_DEST="/data"
 TIMEOUT=120
-STRACE_CMD="timeout -s KILL $TIMEOUT strace -o $MNT_DEST/strace -f -e trace=file -u nobody"
+STRACE_CMD="timeout -s KILL $TIMEOUT strace -o ${RUN_THIS_DEST}/strace -f -e trace=file -u nobody"
 POST_RUN_HOOK="/bin/echo o > /proc/sysrq-trigger"
 
-check_new() {
-    mnt_line=$(blkid | grep 'LABEL="RUN_THIS"')
+
+#
+# See if a disk label exists and mount it
+#
+# @param $1: Label of what to look for
+# @param $2: Mount point, if specified label exists
+#
+check_and_mount_label() {
+    LABEL=$1
+    DEST=$2
+
+    mnt_line=$(blkid | grep 'LABEL="'${LABEL}'"')
     if [ -n "$mnt_line" ]; then
         dir_to_mnt=${mnt_line%%:*}
-        mount $dir_to_mnt $MNT_DEST
+        if [ ! -d "$DEST" ]; then
+            mkdir -p $DEST
+        fi
+        mount ${dir_to_mnt} ${DEST}
         return 0
     else
         return 1
     fi
 }
 
+#
+# Look for new filesystems.
+#
+check_new() {
+    ret_status=1
 
+    if check_and_mount_label "RUN_THIS" "${RUN_THIS_DEST}"; then
+        ret_status=0
+    fi
+    check_and_mount_label "DATA" "${DATA_DEST}"
+
+    return ${ret_status}
+}
+
+
+#
+# Update the status file
+#
+# @param 1: Label of status
+#
 update_status() {
     if [ -n "$1" ]; then
         echo $1 > $STATUS_FILE
@@ -29,58 +73,68 @@ update_status() {
     fi
 }
 
+#
+# Go out and execute code
+#
 run_code() {
-    if [ ! -x "$MNT_DEST/prog" ]; then
+    if [ ! -x "${RUN_THIS_DEST}/prog" ]; then
         echo "No program provided" 1>&2
         return 1
     fi
-    if [ -e "$MNT_DEST/0" ]; then
-        cat $MNT_DEST/0 | $STRACE_CMD $MNT_DEST/prog 1> $MNT_DEST/1 2> $MNT_DEST/2
+    if [ -e "${RUN_THIS_DEST}/0" ]; then
+        cat ${RUN_THIS_DEST}/0 | ${STRACE_CMD} ${RUN_THIS_DEST}/prog 1> ${RUN_THIS_DEST}/1 2> ${RUN_THIS_DEST}/2
     else
-        $STRACE_CMD $MNT_DEST/prog 1> $MNT_DEST/1 2> $MNT_DEST/2
+        ${STRACE_CMD} ${RUN_THIS_DEST}/prog 1> ${RUN_THIS_DEST}/1 2> ${RUN_THIS_DEST}/2
     fi
     copy_files
-    umount $MNT_DEST
+    umount ${RUN_THIS_DEST}
 
-    if [ -n "$POST_RUN_HOOK" ]; then
-        eval $POST_RUN_HOOK
+    if [ -n "${POST_RUN_HOOK}" ]; then
+        eval ${POST_RUN_HOOK}
     fi
 }
 
+#
+# Go out and execute code
+#
 copy_files() {
     count=0
-    for f in $(grep -o -P 'open\("[^"]*".*WR' $MNT_DEST/strace | cut -d\" -f2); do
-        mkdir -p $MNT_DEST/files/$count
-        cp $f $MNT_DEST/files/$count
+    for f in $(grep -o -P 'open\("[^"]*".*WR' $RUN_THIS_DEST/strace | cut -d\" -f2); do
+        mkdir -p ${RUN_THIS_DEST}/files/${count}
+        cp $f ${RUN_THIS_DEST}/files/${count}
         count=$((count+1))
     done
 }
 
+#
+# Go out and execute code
+#
 main_loop() {
-    STATUS="WAITING"
+    STATUS=${WAITING}
     update_status $STATUS
     while true; do
         case $STATUS in
-        $WAITING)
+        ${WAITING})
             if check_new; then
-                STATUS=$RUNNING
+                STATUS=${RUNNING}
                 update_status $STATUS
             else
                 sleep 1
             fi
             ;;
-        $RUNNING)
+        ${RUNNING})
             run_code
-            STATUS="WAITING"
+            STATUS=${WAITING}
             update_status $STATUS
             ;;
         *)
             echo "Invalid status $STATUS" 1>&2
-            STATUS="WAITING"
+            STATUS=${WAITING}
             update_status $STATUS
             ;;
         esac
     done 
 }
 
+# default start
 main_loop
